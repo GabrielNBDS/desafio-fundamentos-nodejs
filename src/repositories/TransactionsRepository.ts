aaecb1e36c6b0e1788b54637de6c414780c95e71
Gabriel Nascimento <gabriel.nbds@gmail.com>
2020-04-15 23:12:43 -0400

    Mission Complete

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    const transactionResponse = this.transactions;
+
+    return transactionResponse;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const reducer = (accumulator: number, currentValue: number): number =>
+      accumulator + currentValue;
+
+    const incomeArr = this.transactions.map(item =>
+      item.type === 'income' ? item.value : 0,
+    );
+
+    const outcomeArr = this.transactions.map(item =>
+      item.type === 'outcome' ? item.value : 0,
+    );
+
+    const income = incomeArr.reduce(reducer);
+    const outcome = outcomeArr.reduce(reducer);
+
+    const total = income - outcome;
+
+    const balance: Balance = { income, outcome, total };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
